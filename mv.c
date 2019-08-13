89f93ce9718e174197e112a5b246442708c6fdd8
Christopher Rose <christophervrose@gmail.com>
2019-08-14 00:00:16 +0530

    Implement mv

--- mv.c
+++ mv.c
@@ -10,5 +10,40 @@ int main(int argc, char* argv[]){
     if(argc>3){
         printf("W:Too many arguments:\nIgnoring arguments not required\n",argc);
     }
-    
+
+    FILE* op = fopen(argv[1],"rb");
+    if(op==NULL){
+        fprintf(stderr,"E:Source file could not be opened\n");
+        return 2;
+    }
+    else{
+        FILE* np = fopen(argv[2],"wb+");
+        if(np==NULL){
+            fprintf(stderr,"E:Destination file cannot be used\n");
+            return 3;
+        }
+        else{
+            char buffer[BSIZE];
+            long int lastCopySize=0,copiedBytes=0;
+            while(!feof(op)){
+                lastCopySize = fread(buffer,1,BSIZE,op);
+                copiedBytes+=lastCopySize;
+                fwrite(buffer,1,lastCopySize,np);
+            }
+            bSize pS = prettySize(copiedBytes);
+            printf("I:Moved: %0.2f%c bytes\n",pS.truncatedSize,pS.modifier);
+            // Now to delete old file
+            fclose(np);
+            if(!remove(argv[1])){
+                printf("I:%s removed\n",argv[1]);
+            }
+            else{
+                fprintf(stderr,"E:Could not remove old file\n");
+                return 4;
+            }
+
+        }
+        fclose(op);
+    }
+    return 0;
 }
