540f7c2206f0d16aa8c73d32bcb597def9ba63de
Christopher Rose <christophervrose@gmail.com>
2019-08-14 23:41:51 +0530

    Update rm, add argument list check

--- src/rm.c
+++ src/rm.c
@@ -1,8 +1,13 @@
 #include<stdio.h>
-
+#include<gen.h>
 //Every file in argv gets eradicated
 
+
+
 int main(int argc, char* argv[]){
+    if(compareargc(argc,2)==-1){
+        fprintf(stderr,"E:Too few inputs - Specify atleast one filename");
+    }
     int statusflag=0,laststatus=0;
     for(int i=1;i<argc;i++){
         laststatus = remove(argv[i])==-1;
