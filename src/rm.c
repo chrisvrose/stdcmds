e51f769c09e74b0ec95b8e6f1d3a2dd3393fd863
Christopher Rose <christophervrose@gmail.com>
2019-08-18 13:12:23 +0530

    Fix arguments in rm

--- src/rm.c
+++ src/rm.c
@@ -1,18 +1,19 @@
 #include<stdio.h>
 #include<gen.h>
+
 //Every file in argv gets eradicated, hopefully
 
 
 
 int main(int argc, char* argv[]){
-    if(compareargc(argc,2)==-1){
+    if(compareargc(getRealArgs(argv,argc),2)==-1){
         fprintf(stderr,"E:Too few inputs - Specify atleast one filename");
     }
     int statusflag=0,laststatus=0;
     unsigned char getSetBits = getSet(argv,argc);
     for(int i=1;i<argc;i++){
         ///Skip flags
-        if(argv[i][0]=='-'){
+        if(isFlag(argv[i])){
             continue;
         }
         laststatus = remove(argv[i])==-1;
